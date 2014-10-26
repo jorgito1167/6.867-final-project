ab2f8e57e0d36e1e5b2c2a6b029dc65d12097fbc
jorgito1167 <jorgito@mit.edu>
2014-10-26 06:04:25 -0400

    Added visualization for counts vs features

--- data_extraction.py
+++ data_extraction.py
@@ -16,12 +16,33 @@ def convert_date(date):
     time = d.hour + d.minute/60.0 + d.second/3600.0
     return time
     
-def time_vs_count(df):
-    counts = []
-    for i in xrange(24):
-        c = df['count'][df['time'] == i]
+def discrete_vs_count(df, field):
+    '''
+    All of the features have a fairly small set of possible values
+    df_train = pd.read_csv('../train.csv', header=0)
+    de.discrete_vs_count(df_train, 'time')
+    '''
+    counts = []  
+    vals =sorted(df[field].unique())
+    for i in vals:
+        c = df['count'][df[field] == i]
         counts.append(c.sum()/len(c))
-    plt.plot(range(24), counts)
+    return vals, counts
+def weather_vs_count(df):
+    vals = df['weather'].unique()
 
-def visualize_date(df_train):
-    return 0
+if __name__ == '__main__':
+    df_train = pd.read_csv('../train.csv', header=0)
+    df_train['time'] = df_train['datetime'].apply(convert_date)
+    features = ['time', 'season', 'holiday', 'workingday', 'weather', 'temp',
+                'atemp', 'humidity', 'windspeed']
+    f, axarr = plt.subplots(3, 3)
+    for i in xrange(3):
+        for j in xrange(3):
+            index = 3*i + j
+            x,y = discrete_vs_count(df_train, features[index])
+            axarr[i,j].scatter(x,y)
+            axarr[i,j].plot(x,y)
+            axarr[i,j].set_title("counts vs " + features[index]) 
+    plt.show()
+  
