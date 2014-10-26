176dc993ce72317ca0f1cc62c96b08070f98b443
jorgito1167 <jorgito@mit.edu>
2014-10-26 06:42:17 -0400

    Trained using elastic nets, score of 1.22473

--- data_extraction.py
+++ data_extraction.py
@@ -1,13 +1,24 @@
 import pandas as pd
 import datetime
 import matplotlib.pyplot as plt
+from sklearn import linear_model
 
 def run():
     df_train = pd.read_csv('../train.csv', header=0)
     df_test = pd.read_csv('../test.csv', header=0)
     df_train['time'] = df_train['datetime'].apply(convert_date)
     df_test['time'] = df_test['datetime'].apply(convert_date)
-    
+    features = ['time', 'season', 'holiday', 'workingday', 'weather', 'temp',
+                'atemp', 'humidity', 'windspeed']
+    x = df_train[features]
+    y = df_train['count']
+    model = linear_model.ElasticNetCV(n_jobs = 3)
+    model.fit(x,y)
+    x_test = df_test[features]
+    df_test['count'] = model.predict(x_test)
+    df_test['count'][df_test['count']<0] = 0
+    output = df_test[['datetime', 'count']]
+    output.to_csv('out.csv', index = False)
 
 
 
