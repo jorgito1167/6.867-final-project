20fcc7dd60ff92312bb9cd0c2e0344299b3f7535
jorgito1167 <jorgito@mit.edu>
2014-10-26 15:28:05 -0400

    Tried making season and weather monotonically increase or decrease

--- data_extraction.py
+++ data_extraction.py
@@ -14,9 +14,16 @@ def run():
     df_test['time'] = df_test['datetime'].apply(convert_date)
     features = ['time', 'season', 'holiday', 'workingday', 'weather', 'temp',
                 'atemp', 'humidity', 'windspeed']
-                
+    
+    change_season(df_train)
+    change_weather(df_train)
+    
+    change_season(df_test)
+    change_weather(df_test)
+    
     x = df_train[features]
     y = df_train['count']
+    
     model = linear_model.ElasticNetCV(n_jobs = 3)
     model.fit(x,y)
     
@@ -27,7 +34,21 @@ def run():
     output.to_csv('out.csv', index = False)
 
 
+def change_season(df):
+    new_season = df['season'].copy()
+    new_season[df['season']==2] = 3
+    new_season[df['season']==3] = 4
+    new_season[df['season']==4] = 2
+    df['season'] = new_season
+    return df
 
+def change_weather(df):
+    new_weather = df['weather'].copy()
+    new_weather[df['weather']==3] = 4
+    new_weather[df['weather']==4] = 3
+    df['weather'] = new_weather
+    return df
+    
 def convert_date(date):
     d = datetime.datetime.strptime(date, "%Y-%m-%d %H:%M:%S")
     time = d.hour + d.minute/60.0 + d.second/3600.0
@@ -54,6 +75,8 @@ def visualize():
     df_train['time'] = df_train['datetime'].apply(convert_date)
     features = ['time', 'season', 'holiday', 'workingday', 'weather', 'temp',
                 'atemp', 'humidity', 'windspeed']
+    change_season(df_train)
+    change_weather(df_train)
     f, axarr = plt.subplots(3, 3)
     for i in xrange(3):
         for j in xrange(3):
